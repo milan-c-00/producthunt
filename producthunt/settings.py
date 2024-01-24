21a0a8adf9f426db1106ee0f01256aa7fcad56dd
milan-c-00 <cvijovic.m21@gmail.com>
2024-01-24 13:31:15 +0100

    Final commit, all up and running.

--- producthunt/settings.py
+++ producthunt/settings.py
@@ -129,6 +129,10 @@ STATIC_ROOT = BASE_DIR / 'static'
 
 STATIC_URL = 'static/'
 
+MEDIA_ROOT = BASE_DIR / 'media'
+
+MEDIA_URL = 'media/'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
